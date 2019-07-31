61cf7d436f06f4739cf45fa644f2e22805370f49
Enoch2k2 <enoch2k2@hotmail.com>
2019-07-31 12:06:25 -0500

    added full CRUD to todolistapp

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -10,13 +10,47 @@ class TodosController < ApplicationController
   end
 
   get '/todos/:id' do
-    @todo = Todo.find_by_id(params[:id])
+    set_todo
     erb :'todos/show'
   end
 
   post '/todos' do
-    todo = Todo.create(params)
-    redirect '/todos'
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      redirect '/todos'
+    else
+      @errors = @todo.errors.full_messages
+      erb :'todos/new'
+    end
   end
 
+  get '/todos/:id/edit' do
+    set_todo
+    erb :'todos/edit'
+  end
+
+  patch '/todos/:id' do
+    set_todo
+    if @todo.update(todo_params)
+      redirect "/todos/#{@todo.id}"
+    else
+      @errors = @todo.errors.full_messages
+      erb :'todos/edit'
+    end
+  end
+
+  delete "/todos/:id" do
+    set_todo
+    @todo.destroy
+    redirect "/todos"
+  end
+
+  private
+    def set_todo
+      @todo = Todo.find_by_id(params[:id])
+    end
+
+    def todo_params
+      { title: params[:todo][:title], completed: params[:todo][:completed] }
+    end
 end
